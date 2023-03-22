c3312289a0bbe7242818cae447bbc224a5bea9ca
mayurck291 <mayurck291@gmail.com>
2023-03-22 18:44:30 +0530

    Create approval.groovy

---
+++ vars/approval.groovy
@@ -0,0 +1,24 @@
+def call(allowedApprovers) {
+    def now = new Date()
+    def dayOfWeek = now.format('EEEE')
+    def timeOfDay = now.format('HH:mm:ss')
+                     
+    if (dayOfWeek != 'Saturday' && dayOfWeek != 'Sunday' && timeOfDay >= '08:45:00' && timeOfDay <= '16:15:00') {
+        def approvers = input message: 'Deploy to production?',
+                              ok: 'Approve',
+                              submitterParameter:'APPROVER'
+                              parameters: [
+                                  [
+                                      $class: 'StringParameterDefinition',
+                                      name: 'APPROVER',
+                                      defaultValue: allowedApprovers.join(','),
+                                      description: 'Comma-separated list of allowed approvers'
+                                  ]
+                              ]
+                                                
+        def authorized = approvers in allowedApprovers
+        if (!authorized) {
+            error "User $approvers is not authorized to approve the deployment"
+        }
+    }
+}
